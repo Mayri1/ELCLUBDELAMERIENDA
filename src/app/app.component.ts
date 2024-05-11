7d11a34234349cfa2ff436a0dbfbd8e917f77e15
Mayra <mayramoisesvega@gmail.com>
2024-05-11 18:58:16 -0300

    modificar submodulo

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService, Item } from './api.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],  // Asegúrate de incluir CommonModule y HttpClientModule
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  items: Item[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getItems().subscribe((data: Item[]) => {
+      this.items = data;
+    }, error => {
+      console.error('Error fetching items', error); // Log de errores
+    });
+  }
+}
