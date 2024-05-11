1fd500734e4764ea5e1113352fdfb06298b25251
Mayra <mayramoisesvega@gmail.com>
2024-05-11 18:42:56 -0300

    commit inicial para Django

---
+++ GYM/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
