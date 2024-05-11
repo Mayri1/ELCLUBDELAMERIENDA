8a47d731f0f8542b221eafeaae64dbed3ff4f03d
May <89091895+Mayri1@users.noreply.github.com>
2024-05-11 18:22:35 -0300

    Delete ELCLUBDELAMERIENDA directory

--- ELCLUBDELAMERIENDA/GYM/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Item
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = '__all__'
