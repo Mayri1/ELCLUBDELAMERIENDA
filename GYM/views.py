a946e7c1bfc7f053424ec013410b310b6aedf23f
May <89091895+Mayri1@users.noreply.github.com>
2024-05-11 19:15:02 -0300

    Delete GYM directory

--- GYM/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Item
-from .serializers import ItemSerializer
-
-class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
