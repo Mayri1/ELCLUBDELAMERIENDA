af06e636e295f5bb0681f3b9131ced196f047f25
josecolombotk <94802771+josecolombotk@users.noreply.github.com>
2024-05-12 21:00:16 -0300

    creacion de landing page

--- angularApp/src/app/app.component.ts
+++ angularApp/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService, Item } from './api.service';
-
+import { LandingPageComponent } from './landing-page/landing-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],  // Asegúrate de incluir CommonModule y HttpClientModule
+  imports: [CommonModule, HttpClientModule, LandingPageComponent],  // Asegúrate de incluir CommonModule y HttpClientModule
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
