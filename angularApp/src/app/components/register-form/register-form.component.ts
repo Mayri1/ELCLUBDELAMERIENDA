1bffe8c84cdb0a1f747406e43ee592b7a44e8e1a
Matias Gonzalez <mng_epson@hotmail.com>
2024-05-26 21:16:45 -0300

    feat: add changes to register form

--- angularApp/src/app/components/register-form/register-form.component.ts
+++ angularApp/src/app/components/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -11,15 +11,30 @@ import { RouterLink } from '@angular/router';
 })
 export class RegisterFormComponent {
 
-  userForm: FormGroup;
-  constructor(){
-    this.userForm = new FormGroup({
-      firstName: new FormControl("", [Validators.required]),
-      lastName: new FormControl("", [Validators.required, Validators.minLength(4)]),
-      userName: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      email: new FormControl("", [Validators.required, Validators.email]),
-      password: new FormControl("", [Validators.required]),
-      isAgree: new FormControl(false)
-    })
+  private readonly _formBuilder = inject(FormBuilder);
+
+  formGroup = this._formBuilder.nonNullable.group({
+    firstName:["", Validators.required],
+    lastName: ["", Validators.required],
+    userName: ["", Validators.required],
+    email: ["", Validators.required, Validators.email],
+    password: ["", Validators.required],
+
+  })
+
+
+  // formGroup = new FormGroup({
+  //   firstName: new FormControl("", [Validators.required]),
+  //   lastName: new FormControl("", [Validators.required, Validators.minLength(4)]),
+  //   userName: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+  //   email: new FormControl("", [Validators.required, Validators.email]),
+  //   password: new FormControl("", [Validators.required]),
+  //   isAgree: new FormControl(false)
+  // })
+
+  clickRegister(): void {
+    const firstName = this.formGroup.controls.firstName.value;
+    console.log(firstName);
+    
   }
 }
