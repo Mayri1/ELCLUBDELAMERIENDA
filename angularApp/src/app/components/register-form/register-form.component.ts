c763832c057865e9b07348d88269c176aca4617c
RocioAvila <rocioa.avila98@gmail.com>
2024-06-14 14:09:28 -0300

    guardado de cambios

--- angularApp/src/app/components/register-form/register-form.component.ts
+++ angularApp/src/app/components/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
